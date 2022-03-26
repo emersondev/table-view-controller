16cb5344e76ecc31f2ae4c02afec42ffe058465e
Emerson Novais <emersonnovais93@gmail.com>
2022-03-25 23:41:57 -0300

    Finish learn basic view table controller
    Finish learn basic view table controller

--- table-view-controller/ViewController.swift
+++ table-view-controller/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var data: [String] = ["Hotdog", "Pizza", "Hamburger"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellReuse = "cellReuse"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuse, for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = data[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+     }
 }
 
